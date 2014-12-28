3a15201b8790bb8c82aaa56c7e9622a4f17b6662
Carl <r4z0r49@gmail.com>
2014-12-28 17:26:01 +0000

    [Version] - Build update for test build

--- SQF/dayz_code/Configs/rscTitles.hpp
+++ SQF/dayz_code/Configs/rscTitles.hpp
@@ -368,7 +368,7 @@ class RscDisplayMain : RscStandardDisplay
 		class DAYZ_Version : CA_Version
 		{
 			idc = -1;
-			text = "@DayZMod 1.8.3";
+			text = "@DayZMod 1.8.3.37404ac";
 			y = "(SafeZoneH + SafeZoneY) - (1 - 0.95)";
 		};
 		delete CA_TitleMainMenu;
--- SQF/dayz_code/config.cpp
+++ SQF/dayz_code/config.cpp
@@ -27,7 +27,7 @@ class CfgMods {
 		hidePicture = 0;
 		hideName = 0;
 		action = "http://www.dayzmod.com";
-		version = "DayZ 1.8.3";
+		version = "DayZ 1.8.3.37404ac";
 		hiveVersion = 0.96;
 		requiredAddons[] = {"Chernarus"};
 	};
