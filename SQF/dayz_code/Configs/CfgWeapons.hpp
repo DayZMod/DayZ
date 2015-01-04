b54e68e2b33d88bff79a2c5404581f1215e22b25
Carl <r4z0r49@gmail.com>
2015-01-04 19:17:25 +0000

    Config tidy up

--- SQF/dayz_code/Configs/CfgWeapons.hpp
+++ SQF/dayz_code/Configs/CfgWeapons.hpp
@@ -28,26 +28,17 @@ class CfgWeapons {
 	class M14_EP1;
 	
 	#include "CfgWeapons\Melee\MeleeMachete.hpp"
-	#include "CfgWeapons\Melee\ItemMachete.hpp"
 	#include "CfgWeapons\Melee\MeleeHatchet.hpp"
-	#include "CfgWeapons\Melee\ItemHatchet.hpp"
 	#include "CfgWeapons\Melee\MeleeCrowbar.hpp"
-	#include "CfgWeapons\Melee\ItemCrowbar.hpp"
 	//#include "CfgWeapons\Melee\Crossbow.hpp"
 	#include "CfgWeapons\Melee\MeleeBaseBallBat.hpp"
 	#include "CfgWeapons\Melee\MeleeBaseBallBatBarbed.hpp"
 	#include "CfgWeapons\Melee\MeleeBaseBallBatNails.hpp"
 	#include "CfgWeapons\Melee\MeleeFishingPole.hpp"
 	
-	#include "CfgWeapons\Item\ItemWatch.hpp"
-	#include "CfgWeapons\Item\ItemMap.hpp"
-	#include "CfgWeapons\Item\ItemMap_Debug.hpp"
-	#include "CfgWeapons\Item\ItemCompass.hpp"
-	#include "CfgWeapons\Item\Flashlight.hpp"
-	#include "CfgWeapons\Item\Flare.hpp"
-	#include "CfgWeapons\Item\ItemEtool.hpp"
-	#include "CfgWeapons\Item\ItemShovel.hpp"
-	#include "CfgWeapons\Item\ItemFishingPole.hpp"
+	#include "CfgWeapons\Melee\Flare.hpp"
+	#include "CfgWeapons\Melee\Flashlight.hpp"
+
 	//#include "CfgWeapons\Item\ItemMatchbox.hpp" Moved to equip
 	
 	//Pistols
