64274c600598f44433c06e92b557030da4913de0
Carl <r4z0r49@gmail.com>
2015-01-04 19:14:14 +0000

    [New Tools] - Pickaxe & Hammer

---
+++ SQF/dayz_communityweapons/config.cpp
@@ -0,0 +1,10 @@
+//Bined Weapon models only
+
+class CfgPatches {
+	class dayz_communityweapons {
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 0.1;
+		requiredAddons[] = {"CAWeapons", "dayz_weapons", "dayz_equip"};
+	};
+};
