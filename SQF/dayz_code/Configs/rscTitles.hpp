faf3e016a7b42d11ac6458dc8b590f74c57f2a0e
Carl <r4z0r49@gmail.com>
2015-01-09 19:51:00 +0000

    [Release] - 100aa50 Pushing to testers
    Pushing to Testers weekend

--- SQF/dayz_code/Configs/rscTitles.hpp
+++ SQF/dayz_code/Configs/rscTitles.hpp
@@ -370,7 +370,7 @@ class RscDisplayMain : RscStandardDisplay
 		class DAYZ_Version : CA_Version
 		{
 			idc = -1;
-			text = "@DayZMod 1.8.3-a216b06";
+			text = "@DayZMod 1.8.3-100aa50";
 			y = "(SafeZoneH + SafeZoneY) - (1 - 0.95)";
 		};
 		delete CA_TitleMainMenu;
--- SQF/dayz_code/config.cpp
+++ SQF/dayz_code/config.cpp
@@ -27,7 +27,7 @@ class CfgMods {
 		hidePicture = 0;
 		hideName = 0;
 		action = "http://www.dayzmod.com";
-		version = "DayZ 1.8.3-a216b06";
+		version = "DayZ 1.8.3-100aa50";
 		hiveVersion = 0.96;
 		requiredAddons[] = {"Chernarus"};
 	};
