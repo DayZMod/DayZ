1650068b8423c96d12963c16d3d105cae3099999
Carl <r4z0r49@gmail.com>
2015-01-25 21:28:48 +0000

    Version updated.

--- SQF/dayz_code/Configs/rscTitles.hpp
+++ SQF/dayz_code/Configs/rscTitles.hpp
@@ -370,7 +370,7 @@ class RscDisplayMain : RscStandardDisplay
 		class DAYZ_Version : CA_Version
 		{
 			idc = -1;
-			text = "@DayZMod 1.8.3-1facc9e";
+			text = "@DayZMod 1.8.3-110fa33";
 			y = "(SafeZoneH + SafeZoneY) - (1 - 0.95)";
 		};
 		delete CA_TitleMainMenu;
--- SQF/dayz_code/config.cpp
+++ SQF/dayz_code/config.cpp
@@ -27,7 +27,7 @@ class CfgMods {
 		hidePicture = 0;
 		hideName = 0;
 		action = "http://www.dayzmod.com";
-		version = "DayZ 1.8.3-1facc9e";
+		version = "DayZ 1.8.3-110fa33";
 		hiveVersion = 0.96;
 		requiredAddons[] = {"Chernarus"};
 	};
