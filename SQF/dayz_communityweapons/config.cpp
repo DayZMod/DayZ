738ac919c4837b4c2256d1b48aab35986687af5c
Carl <r4z0r49@gmail.com>
2015-01-26 09:25:41 +0000

    Moved pickaxe and hammer back to equip.

--- SQF/dayz_communityweapons/config.cpp
+++ SQF/dayz_communityweapons/config.cpp
@@ -16,69 +16,6 @@ class FullAuto;
 
 class cfgWeapons {
 	class Rifle;
-	class ItemCore;
-	
-	class ItemPickaxe : ItemCore {
-		scope = public;
-		displayName = $STR_EQUIP_NAME_PICKAXE;
-		model = "z\addons\dayz_communityweapons\models\pickaxe\pickaxe.p3d";
-		picture = "\z\addons\dayz_communityweapons\models\pickaxe\pickaxe.paa";
-		descriptionShort = $STR_EQUIP_DESC_PICKAXE;
-		
-		class ItemActions {
-			class Use
-			{
-				text="Harvest Stone";
-				script="spawn player_mineStone;";
-			};
-		};
-	};
-	
-	class ItemPickaxeBroken : ItemCore {
-		scope = public;
-		model = "z\addons\dayz_communityweapons\models\pickaxe\pickaxe.p3d";
-		picture = "\z\addons\dayz_communityweapons\models\pickaxe\pickaxe.paa";
-		displayName = $STR_name_ItemPickaxeBroken; //"Broken Hatchet";
-		descriptionShort = $STR_desc_ItemPickaxeBroken; //"Too much force has broken this Hatchet. Hopefully you find something to repair it.";
-		fixedTool = "ItemPickaxe";
-		class ItemActions {
-			class Toolbelt {
-				text = "Fix Pickaxe.";
-				script="spawn player_fixHatchet;";
-			};
-		};
-	};
-	
-	class ItemSledgeHammer : ItemCore {
-		scope = public;
-		displayName = $STR_EQUIP_NAME_SledgeHammer;
-		model = "z\addons\dayz_communityweapons\models\sledge_hammer\sledgehammer.p3d";
-		picture = "\z\addons\dayz_communityweapons\models\sledge_hammer\sledgehammer.paa";
-		descriptionShort = $STR_EQUIP_DESC_SledgeHammer;
-		
-		class ItemActions {
-			class Use
-			{
-				text="Harvest Stone";
-				script="spawn player_mineStone;";
-			};
-		};
-	};
-	
-	class ItemSledgeHammerBroken : ItemCore {
-		scope = public;
-		model = "z\addons\dayz_communityweapons\models\sledge_hammer\sledgehammer.p3d";
-		picture = "\z\addons\dayz_communityweapons\models\sledge_hammer\sledgehammer.paa";
-		displayName = $STR_name_ItemSledgeHammerBroken; //"Broken Hatchet";
-		descriptionShort = $STR_desc_ItemSledgeHammerBroken; //"Too much force has broken this Hatchet. Hopefully you find something to repair it.";
-		fixedTool = "ItemSledgeHammer";
-		class ItemActions {
-			class Toolbelt {
-				text = "Fix Sledge Hammer.";
-				script="spawn player_fixHatchet;";
-			};
-		};
-	};
 	
 	#include "configs\mosin.hpp"
 };
