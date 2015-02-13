b66dcca75b26d7f1396ed72a4008f7869fdf08a9
Carl <r4z0r49@gmail.com>
2015-02-13 18:07:37 +0000

    Version updated.

--- SQF/dayz_code/Configs/rscTitles.hpp
+++ SQF/dayz_code/Configs/rscTitles.hpp
@@ -370,7 +370,7 @@ class RscDisplayMain : RscStandardDisplay
 		class DAYZ_Version : CA_Version
 		{
 			idc = -1;
-			text = "@DayZMod 1.8.4";
+			text = "@DayZMod 1.8.4.1";
 			y = "(SafeZoneH + SafeZoneY) - (1 - 0.95)";
 		};
 		delete CA_TitleMainMenu;
--- SQF/dayz_code/config.cpp
+++ SQF/dayz_code/config.cpp
@@ -27,7 +27,7 @@ class CfgMods {
 		hidePicture = 0;
 		hideName = 0;
 		action = "http://www.dayzmod.com";
-		version = "DayZ 1.8.4";
+		version = "DayZ 1.8.4.1";
 		hiveVersion = 0.96;
 		requiredAddons[] = {"Chernarus"};
 	};
