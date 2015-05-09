35ef883f3d160e1e39bde72761ba89874df7a559
AsFoxy <asfoxyy@gmail.com>
2015-05-09 19:39:17 +0300

    Updated vehicle cleanup

---
+++ SQL/new/pCleanup.sql
@@ -0,0 +1,104 @@
+/*!50003 DROP PROCEDURE IF EXISTS `pCleanup` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`dayz`@`localhost` PROCEDURE `pCleanup`()
+BEGIN 
+#Last ran
+	update event_scheduler set LastRun = NOW() where System = "pCleanup";
+
+#starts outofbounds cleanup
+        CALL pCleanupOOB();
+ 
+#remove damaged objects
+        DELETE
+                FROM object_data
+                WHERE CharacterID != 0 AND Damage = 1;
+
+#remove damaged vehicles older than vehicle_spawns.CleanupTime
+	DELETE FROM object_data
+	WHERE CharacterID = 0
+		AND Damage = 1
+		AND SYSDATE() < last_updated + INTERVAL (
+			SELECT CleanupTime
+			FROM vehicle_spawns
+			WHERE vehicle_spawns.Classname = object_data.Classname
+			LIMIT 1) MINUTE;
+
+#remove empty tents older than seven days
+        DELETE
+                FROM object_data
+                WHERE (Classname like 'TentStorage%' or Classname like 'StashSmall%' or Classname like 'StashMedium%' or Classname like 'DomeTentStorage%')
+                        AND DATE(last_updated) < CURDATE() - INTERVAL 7 DAY
+                        AND Inventory = '[[[],[]],[[],[]],[[],[]]]';
+       
+        DELETE
+                FROM object_data
+                WHERE (Classname like 'TentStorage%' or Classname like 'StashSmall%' or Classname like 'StashMedium%' or Classname like 'DomeTentStorage%')
+                        AND DATE(last_updated) < CURDATE() - INTERVAL 7 DAY
+                        AND Inventory = '[]';          
+ 
+#remove barbed wire older than two days
+        DELETE
+            FROM object_data
+				WHERE Classname = 'Wire_cat1'
+					AND DATE(last_updated) < CURDATE() - INTERVAL 2 DAY;
+					                      
+#remove Tank Traps older than fifteen days
+        DELETE
+                FROM object_data
+                WHERE Classname = 'Hedgehog_DZ'
+                        AND DATE(last_updated) < CURDATE() - INTERVAL 15 DAY;
+ 
+#remove Sandbags older than twenty days
+        DELETE
+                FROM object_data
+                WHERE Classname = 'Sandbag1_DZ'
+                        AND DATE(last_updated) < CURDATE() - INTERVAL 20 DAY;
+ 
+#remove Traps older than five days
+        DELETE
+                FROM object_data
+                WHERE (Classname = 'BearTrap_DZ' or Classname = 'TrapBearTrapFlare' or Classname = 'TrapBearTrapSmoke' or Classname = 'Trap_Cans' or Classname = 'TrapTripwireFlare' or Classname = 'TrapTripwireGrenade' or Classname = 'TrapTripwireSmoke')
+                        AND DATE(last_updated) < CURDATE() - INTERVAL 5 DAY; 
+                        
+#remove incomplete fences after 1 Day								
+        DELETE
+                FROM object_data
+                WHERE (Classname = 'WoodenFence_1_foundation')
+                        AND DATE(last_updated) < CURDATE() - INTERVAL 1 Day;  
+                        
+#remove incomplete fences after 3 Dayz								
+        DELETE
+                FROM object_data
+                WHERE (Classname = 'WoodenFence_1_frame' or Classname = 'WoodenFence_quaterpanel' or Classname = 'WoodenFence_halfpanel' or Classname = 'WoodenFence_thirdpanel')
+                        AND DATE(last_updated) < CURDATE() - INTERVAL 3 Day; 
+                       
+#remove dead players from data table
+        DELETE
+                FROM character_data
+                WHERE Alive=0 AND DATE(last_updated) < CURDATE() - INTERVAL 90 Day; 
+#Remove Bad Data
+				DELETE
+								FROM object_data
+								WHERE Classname like '%_base';
+			
+#Remove wire if the owner has died.						
+		DELETE
+			FROM Object_DATA
+			USING Object_DATA, Character_DATA
+				WHERE Object_DATA.Classname = 'Wire_cat1'
+					AND Object_DATA.CharacterID = Character_DATA.CharacterID
+					AND Character_DATA.Alive = 0;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
