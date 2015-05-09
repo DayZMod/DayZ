44102e5bf86670fc8085e76516776839bb440b88
Carl <r4z0r49@gmail.com>
2015-05-09 12:55:06 +0100

    Updated Legacy Attachments
    Remove next patch 1.8.7

--- SQF/dayz_code/Configs/CfgMagazines/Attachments/legacy/Attachment_CrossbowCCO.hpp
+++ SQF/dayz_code/Configs/CfgMagazines/Attachments/legacy/Attachment_CrossbowCCO.hpp
@@ -2,20 +2,9 @@
 
 class Attachment_Crossbow_CCO : CA_Magazine {
 	scope = public;
-	displayName = $STR_ATTACHMENT_NAME_CCO_RED_CROSSBOW;
-	descriptionShort = $STR_ATTACHMENT_DESC_CCO_RED_CROSSBOW;
+	displayName = "Broken Attachment";
+	descriptionShort = "A Broken Attachment not worth holding onto.";
 	count = 1;
 	picture = "\z\addons\dayz_communityassets\pictures\attachment_aim.paa";
 	type = 256;
-	/*
-	oringal = "Crossbow_DZ";
-	weapon = "Crossbow_CCO";
-	
-	class ItemActions {
-		class Use {
-			text = $STR_ACTIONS_ATTACH_CROSSBOW;
-			script = "spawn player_useAttchment;";
-		};
-	};
-	*/
 };
--- SQF/dayz_code/Configs/CfgMagazines/Attachments/legacy/Attachment_SniperScope.hpp
+++ SQF/dayz_code/Configs/CfgMagazines/Attachments/legacy/Attachment_SniperScope.hpp
@@ -1,18 +1,8 @@
 class Attachment_M14_SniperScope : CA_Magazine {
 	scope = public;
-	displayName = $STR_ATTACHMENT_NAME_SCOPE_M14;
-	descriptionShort = $STR_ATTACHMENT_DESC_SCOPE_M14;
+	displayName = "Broken Attachment";
+	descriptionShort = "A Broken Attachment not worth holding onto.";
 	count = 1;
 	picture = "\z\addons\dayz_communityassets\pictures\attachment_sniperscope.paa";
 	type = 256;
-	
-	oringal = "M14_EP1";
-	weapon = "DMR_DZ";
-	
-	class ItemActions {
-		class Use {
-			text = $STR_ACTIONS_ATTACH_M14;
-			script = "spawn player_useAttchment;";
-		};
-	};
 };
