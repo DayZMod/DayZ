b0e605b0525ba2b962e89662220d375471f667e7
CoftSock <c0rp3rman1c@gmail.com>
2015-05-12 16:02:08 +1000

    Create Ghillie.hpp
    recomend lower camo net loot spawn (camo nets very rarely have much use in game)

---
+++ SQF/dayz_code/Configs/CfgCrafting/Recipes/Other/Ghillie.hpp
@@ -0,0 +1,16 @@
+class Blueprint_Binoculars : Recipe {
+	displayName = $STR_CRAFT_NAME_Binoculars;
+	input[] = 
+	{
+		{"WeaponHolder_ItemCamoNet","CfgMagazines",1},
+		{"equip_rag","CfgMagazines",1}
+	};
+	output[] = 
+	{
+		{"Skin_Sniper1_DZ","CfgMagazines",2}
+	};
+	required[] = 
+	{
+		{"ItemKnife","CfgWeapons",1}
+	};
+
