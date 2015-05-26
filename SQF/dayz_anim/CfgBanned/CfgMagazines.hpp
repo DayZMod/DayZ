31c2ee82744a25375af420a767b87f3794e210f5
AsFoxy <asfoxyy@gmail.com>
2015-05-26 13:02:26 +0300

    Added GP-25 ammo and AKSD mags.

--- SQF/dayz_anim/CfgBanned/CfgMagazines.hpp
+++ SQF/dayz_anim/CfgBanned/CfgMagazines.hpp
@@ -67,8 +67,8 @@ class CfgMagazines {
 	/*class FlareWhite_GP25: FakeMagazine {};
 	class FlareGreen_GP25: FakeMagazine {};
 	class FlareRed_GP25: FakeMagazine {};
-	class FlareYellow_GP25: FakeMagazine {};*/
-	class 1Rnd_SMOKE_GP25: FakeMagazine {};
+	class FlareYellow_GP25: FakeMagazine {};
+	class 1Rnd_SMOKE_GP25: FakeMagazine {};*/
 	class 1Rnd_SmokeRed_GP25: FakeMagazine {};
 	class 1Rnd_SmokeGreen_GP25: FakeMagazine {};
 	class 1Rnd_SmokeYellow_GP25: FakeMagazine {};
