9c1920d585d8f7f180471c1fb27954159c616db3
AsFoxy <asfoxyy@gmail.com>
2015-06-07 22:15:08 +0300

    Removed legacy attachments

--- SQF/dayz_code/Configs/CfgMagazines/Attachments/legacy/Attachment_Camo.hpp
+++
@@ -1,8 +0,0 @@
-class Attachment_M4A1_Aim_camo : CA_Magazine {
-	scope = public;
-	displayName = "Broken Attachment";
-	descriptionShort = "A Broken Attachment not worth holding onto.";
-	count = 1;
-	picture = "\z\addons\dayz_communityassets\CraftingPlaceHolders\equip_part_camo.paa";
-	type = 256;
-};
--- SQF/dayz_code/Configs/CfgMagazines/Attachments/legacy/Attachment_Kobra.hpp
+++
@@ -1,8 +0,0 @@
-class Attachment_Kobra : CA_Magazine {
-	scope = public;
-	displayName = "Broken Attachment";
-	descriptionShort = "A Broken Attachment not worth holding onto.";
-	count = 1;
-	picture = "\z\addons\dayz_communityassets\pictures\attchement_kobra.paa";
-	type = 256;
-};
