8d268dd275662c687ff702ad16133f9646b64d11
AsFoxy <asfoxyy@gmail.com>
2015-06-21 22:33:42 +0300

    Fixed SVD Ghillie icon and name

--- SQF/dayz_code/Configs/CfgWeapons/Rifles/SVD.hpp
+++ SQF/dayz_code/Configs/CfgWeapons/Rifles/SVD.hpp
@@ -1,5 +1,9 @@
 class SVD_DZ : SVD
 {
+	model = "ca\weapons\svd_dragunov_proxy.p3d";
+	picture = "ca\weapons\data\equip\w_svd_ca.paa";
+	displayName = $STR_DZ_WPN_SVD_NAME;
+	
 	magazines[] = {10Rnd_762x54_SVD};
 	
 	reloadTime = 0.06;
@@ -18,6 +22,8 @@ class SVD_DZ : SVD
 class SVD_Gh_DZ : SVD_DZ
 {
 	model = "ca\weapons\svd_camo.p3d";
+	picture = "ca\weapons\data\equip\w_svd_camo_ca.paa";
+	displayName = $STR_DZ_WPN_SVD_GH_NAME;
 	
 	class Attachments {};
 	
