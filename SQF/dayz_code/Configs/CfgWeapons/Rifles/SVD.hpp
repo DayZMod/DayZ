076cbbf6651782896d3e89b9552e03f86831c5ea
Carl <r4z0r49@gmail.com>
2015-07-28 19:33:06 +0100

    fixed forward slash in svd

--- SQF/dayz_code/Configs/CfgWeapons/Rifles/SVD.hpp
+++ SQF/dayz_code/Configs/CfgWeapons/Rifles/SVD.hpp
@@ -1,7 +1,7 @@
 class SVD_DZ : SVD
 {
 	model = "ca\weapons\svd_dragunov_proxy.p3d";
-	picture = "ca\weapons\data\equip\w_svd_ca.paa";
+	picture = "\ca\weapons\data\equip\w_svd_ca.paa";
 	displayName = $STR_DZ_WPN_SVD_NAME;
 	
 	magazines[] = {10Rnd_762x54_SVD};
@@ -22,7 +22,7 @@ class SVD_DZ : SVD
 class SVD_Gh_DZ : SVD_DZ
 {
 	model = "ca\weapons\svd_camo.p3d";
-	picture = "ca\weapons\data\equip\w_svd_camo_ca.paa";
+	picture = "\ca\weapons\data\equip\w_svd_camo_ca.paa";
 	displayName = $STR_DZ_WPN_SVD_GH_NAME;
 	
 	class Attachments {};
