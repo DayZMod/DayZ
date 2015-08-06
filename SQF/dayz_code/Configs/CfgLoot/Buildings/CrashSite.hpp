c660204e7a532f5cbea16db307a3cd35efd6bb47
Foxy <asfoxyy@gmail.com>
2015-08-07 02:17:35 +0300

    Updated crashsite CfgLoot

--- SQF/dayz_code/Configs/CfgLoot/Buildings/CrashSite.hpp
+++ SQF/dayz_code/Configs/CfgLoot/Buildings/CrashSite.hpp
@@ -7,7 +7,6 @@ class CrashSite : Default
 };
 
 class CrashSite_RU : CrashSite {};
-class CrashSite_US1 : CrashSite {};
-class CrashSite_US2 : CrashSite {};
+class CrashSite_US : CrashSite {};
 class CrashSite_EU : CrashSite {};
 class CrashSite_UN : CrashSite {};
