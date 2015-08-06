6f3f1911fa38fce2beb62f585d1c0df44ab96629
Foxy <asfoxyy@gmail.com>
2015-08-06 16:38:37 +0300

    Fixed ResidentialRuins loot table

--- SQF/dayz_code/Configs/CfgLoot/Groups/Buildings/ResidentialRuins.hpp
+++ SQF/dayz_code/Configs/CfgLoot/Groups/Buildings/ResidentialRuins.hpp
@@ -4,5 +4,5 @@ ResidentialRuins[] =
 	{Loot_MAGAZINE,		10,		ItemStone},
 	{Loot_MAGAZINE,		7,		equip_metal_sheet_rusted},
 	{Loot_GROUP,		4,		Trash},
-	{Loot_MAGAZINE,		3,		AttachmentsGeneric}
+	{Loot_GROUP,		3,		AttachmentsGeneric}
 };
