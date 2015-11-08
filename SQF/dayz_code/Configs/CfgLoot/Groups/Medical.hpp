9a1382f8dfea5e7ceba7077e978471f2a16ebc76
AsFoxy <asfoxyy@gmail.com>
2015-11-08 21:26:53 +0200

    Loot balancing

--- SQF/dayz_code/Configs/CfgLoot/Groups/Medical.hpp
+++ SQF/dayz_code/Configs/CfgLoot/Groups/Medical.hpp
@@ -10,18 +10,15 @@ MedicalLow[] =
 
 MedicalHigh[] =
 {
-	{Loot_MAGAZINE,		8,		ItemBandage},
-	{Loot_MAGAZINE,		5,		ItemPainkiller},
-	{Loot_MAGAZINE,		4,		ItemMorphine},
-	{Loot_MAGAZINE,		3,		ItemEpinephrine},
+	{Loot_MAGAZINE,		5,		ItemMorphine},
 	{Loot_MAGAZINE,		3,		ItemBloodTester},
-//	{Loot_MAGAZINE,		3,		ItemTransfusionKit},
-	{Loot_MAGAZINE,		4,		ItemAntibacterialWipe},
-	{Loot_MAGAZINE,		4,		ItemHeatPack},
-	{Loot_MAGAZINE,		4,		ItemBloodbagEmpty},
+	{Loot_MAGAZINE,		5,		ItemHeatPack},
+	{Loot_MAGAZINE,		2,		ItemBloodbagEmpty},
 	
-	{Loot_GROUP,		1,		Antibiotics},
-	{Loot_GROUP,		5,		Bloodbags}
+	{Loot_GROUP,		2,		Antibiotics},
+	{Loot_GROUP,		6,		Bloodbags},
+	
+	{Loot_GROUP,		7,		MedicalLow}
 };
 
 Antibiotics[] =
@@ -33,15 +30,15 @@ Antibiotics[] =
 
 Bloodbags[] =
 {
-	{Loot_MAGAZINE,		15,		ItemBloodbagAPos},
-	{Loot_MAGAZINE,		10,		ItemBloodbagANeg},
+	{Loot_MAGAZINE,		8,		ItemBloodbagAPos},
+	{Loot_MAGAZINE,		7,		ItemBloodbagANeg},
 	
-	{Loot_MAGAZINE,		15,		ItemBloodbagBPos},
-	{Loot_MAGAZINE,		10,		ItemBloodbagBNeg},
+	{Loot_MAGAZINE,		8,		ItemBloodbagBPos},
+	{Loot_MAGAZINE,		7,		ItemBloodbagBNeg},
 	
 	{Loot_MAGAZINE,		5,		ItemBloodbagABPos},
 	{Loot_MAGAZINE,		5,		ItemBloodbagABNeg},
 	
-	{Loot_MAGAZINE,		25,		ItemBloodbagOPos},
-	{Loot_MAGAZINE,		15,		ItemBloodbagONeg}
+	{Loot_MAGAZINE,		15,		ItemBloodbagOPos},
+	{Loot_MAGAZINE,		25,		ItemBloodbagONeg}
 };
