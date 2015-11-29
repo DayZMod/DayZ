b1baaaf84bafec3c3cc52e50d791fae4dd7ade24
R4Z0R49 <R4Z0R49@gmail.com>
2015-11-29 10:34:46 +0000

    MetalFence Tidy up

--- SQF/dayz_buildings/config.cpp
+++ SQF/dayz_buildings/config.cpp
@@ -1,6 +1,3 @@
-// Devlopment only system - Comment out for stable (Search KeyWord - VERSION)
-#define _Devlopment_
-
 class CfgPatches               
 {      
 	class dayz_buildings    
@@ -22,52 +19,6 @@ class CfgAddons
 		};
 	};
 };
-class CfgMagazines {
-	class CA_Magazine;	// External class reference
-	
-#ifdef _Devlopment_
-	class ItemMetalSheet: CA_Magazine
-	{
-		scope = 2;
-		count = 1;
-		type = 256;
-		displayName = $STR_BLD_name_ItemMetalSheet;//"Corrugated Sheet"
-		picture = "\z\addons\dayz_buildings\equip\item_sheetmetal.paa";
-		model = "z\addons\dayz_buildings\models\sheet_metal.p3d";
-		descriptionShort = $STR_BLD_desc_ItemMetalSheet;//"Metal Sheeting"
-	};		
-	class ItemConcreteBlock: CA_Magazine
-	{
-		scope = 2;
-		count = 1;
-		type = 256;
-		displayName = $STR_BLD_name_ItemConcreteBlock;//"Concrete Block"
-		picture = "\z\addons\dayz_buildings\equip\concreteblock.paa";
-		model = "z\addons\dayz_buildings\models\concreteblock.p3d";
-		descriptionShort = $STR_BLD_desc_ItemConcreteBlock;//"Concrete Block"
-	};	
-	class ItemRSJ: CA_Magazine
-	{
-		scope = 2;
-		count = 1;
-		type = 256;
-		displayName = $STR_BLD_name_ItemRSJ;//"Steel Beam"
-		picture = "\z\addons\dayz_buildings\equip\item_rsj.paa";
-		model = "z\addons\dayz_buildings\models\rsj.p3d";
-		descriptionShort = $STR_BLD_desc_ItemRSJ;//"Steel RSJ Support Beam"
-	};	
-	class ItemScrews: CA_Magazine
-	{
-		scope = 2;
-		count = 1;
-		type = 256;
-		displayName = $STR_BLD_name_ItemScrews;//"Box of screws for metal"
-		picture = "\z\addons\dayz_buildings\equip\item_screws.paa";
-		model = "z\addons\dayz_buildings\models\screws.p3d";
-		descriptionShort = $STR_BLD_desc_ItemScrews;//"Box of screws"
-	};	
-#endif
-};	
 
 class CfgVehicleClasses {
  
@@ -79,54 +30,6 @@ class CfgVehicleClasses {
 class CfgVehicles
 {
 	class WeaponHolderBase;
-/*
-	class WeaponHolder_ItemWorkBench: WeaponHolderBase
-	{
-		scope=2;
-		displayName = $STR_BLD_name_WeaponHolder_ItemWorkBench;//"Workbench"
-		model = "z\addons\dayz_buildings\models\workbench_flat.p3d";
-		class eventHandlers
-		{
-			init="[(_this select 0),'cfgMagazines','ItemWorkBench'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
-		};
-	};
-
-	class WeaponHolder_ItemMetalSheet: WeaponHolderBase
-	{
-		scope=2;
-		displayName = $STR_BLD_name_WeaponHolder_ItemMetalSheet;//"Metal Sheet"
-		model = "z\addons\dayz_buildings\models\sheet_metal.p3d";
-        destrType = "DestructNo";
-		class eventHandlers
-		{
-			init="[(_this select 0),'cfgMagazines','ItemMetalSheet'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
-		};
-	};
-	
-	class WeaponHolder_ItemConcreteBlock: WeaponHolderBase
-	{
-		scope=2;
-		displayName = $STR_BLD_name_WeaponHolder_ItemConcreteBlock;//"Concrete Blocks"
-		model = "z\addons\dayz_buildings\models\concreteblock_wep.p3d";
-        destrType = "DestructNo";
-		class eventHandlers
-		{
-			init="[(_this select 0),'cfgMagazines','ItemConcreteBlock'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
-		};
-	};
-	
-	class WeaponHolder_ItemRSJ: WeaponHolderBase
-	{
-		scope=2;
-		displayName = $STR_BLD_name_WeaponHolder_ItemRSJ;//"Steel Beam"
-		model = "z\addons\dayz_buildings\models\rsj_wep.p3d";
-        destrType = "DestructNo";
-		class eventHandlers
-		{
-			init="[(_this select 0),'cfgMagazines','ItemRSJ'] execVM '\z\addons\dayz_code\init\object_pickupAction.sqf';";
-		};
-	};
-*/
     class House;
 	class DZ_buildables: House
 	{
@@ -246,71 +149,5 @@ class CfgVehicles
 class CfgWeapons 
 {
 	class ItemCore;
-	class ItemDIY_wood: ItemCore {
-		scope = 2;
-		picture = "\z\addons\dayz_buildings\equip\icon_diy_wood.paa";
-		model = "z\addons\dayz_buildings\models\diymanual_wood.p3d";
-		icon = "Ca\misc\data\icons\i_danger_CA.paa";
-		displayName = $STR_BLD_name_ItemDIY_wood;//"DIY Manual (Wood)"
-		descriptionShort = $STR_BLD_desc_ItemDIY_wood;//"For wooden fences"
-	
-	#ifdef _Devlopment_
-		class ItemActions {
-			class Build {
-				text = $STR_BLD_build_ItemDIY_wood;//"Wooden fence"
-				script = "; ['ItemDIY_wood','Build'] spawn player_build; r_action_count = r_action_count + 1;";
-				require[] = {"ItemEtool"};
-				consume[] = {"ItemLog", "ItemStone", "ItemStone"};
-				ghost = "WoodenFence_ghost";
-				create = "WoodenFence_1_foundation";
-			};
-		};
-	#endif
-	};
-	
-	class ItemDIY_Gate: ItemCore {
-		scope = 2;
-		picture = "\z\addons\dayz_buildings\equip\icon_diy_wood.paa";
-		model = "z\addons\dayz_buildings\models\diymanual_wood.p3d";
-		icon = "Ca\misc\data\icons\i_danger_CA.paa";
-		displayName = $STR_BLD_name_ItemDIY_woodGate;//"DIY Manual (Gate's)"
-		descriptionShort = $STR_BLD_desc_ItemDIY_woodGate;//"For wooden Gates"
-	
-	#ifdef _Devlopment_
-		class ItemActions {
-			class Build {
-				text = $STR_BLD_build_ItemDIY_woodGate;//"Wooden Gate"
-				script = "; ['ItemDIY_Gate','Build'] spawn player_build; r_action_count = r_action_count + 1;";
-				require[] = {"ItemEtool"};
-				consume[] = {"ItemLog"};
-				ghost = "WoodenGate_ghost"; //Ghost model needs to be changed to WoodenGate4 so the player sees the finished fully upgrade model as the ghost
-				create = "WoodenGate_foundation"; //Foundation model is using wooden fence foundation (needs to change)
-			};
-
-		};
-	#endif
-	};
-	
-	//Left in place to stop problems with dbs
-	class ItemDIY_metal: ItemCore {
-		scope = 2;
-		picture = "\z\addons\dayz_buildings\equip\icon_diy_metal.paa";
-		model = "z\addons\dayz_buildings\models\diymanual_metal.p3d";
-		icon = "Ca\misc\data\icons\i_danger_CA.paa";
-		displayName = $STR_BLD_name_ItemDIY_metal;//"DIY Manual (Metal)"
-		descriptionShort = $STR_BLD_desc_ItemDIY_metal;//"For Metal fences"
-		
-	#ifdef _Devlopment_
-		class ItemActions {
-			class Build {
-				text = $STR_BLD_build_ItemDIY_metal;//"Metal fence"
-				script = "; ['ItemDIY_metal','Build'] spawn player_build; r_action_count = r_action_count + 1;";
-				require[] = {"ItemEtool"};
-				consume[] = {"ItemRSJ", "ItemStone", "ItemStone" };
-				ghost = "MetalFence_ghost";
-				create = "MetalFence_1_foundation";
-			};
-		};
-	#endif
-	};
+	#include "configs\ItemDIYBooks.hpp"
 };
---
+++ SQF/dayz_buildings/configs/ItemDIYBooks.hpp
@@ -0,0 +1,70 @@
+// Devlopment only system - Comment out for stable (Search KeyWord - VERSION)
+#define _Devlopment_
+
+class ItemDIY_wood: ItemCore {
+	scope = 2;
+	picture = "\z\addons\dayz_buildings\equip\icon_diy_wood.paa";
+	model = "z\addons\dayz_buildings\models\diymanual_wood.p3d";
+	icon = "Ca\misc\data\icons\i_danger_CA.paa";
+	displayName = $STR_BLD_name_ItemDIY_wood;//"DIY Manual (Wood)"
+	descriptionShort = $STR_BLD_desc_ItemDIY_wood;//"For wooden fences"
+
+#ifdef _Devlopment_
+	class ItemActions {
+		class Build {
+			text = $STR_BLD_build_ItemDIY_wood;//"Wooden fence"
+			script = "; ['ItemDIY_wood','Build'] spawn player_build; r_action_count = r_action_count + 1;";
+			require[] = {"ItemEtool"};
+			consume[] = {"ItemLog", "ItemStone", "ItemStone"};
+			ghost = "WoodenFence_ghost";
+			create = "WoodenFence_1_foundation";
+		};
+	};
+#endif
+};
+
+class ItemDIY_Gate: ItemCore {
+	scope = 2;
+	picture = "\z\addons\dayz_buildings\equip\icon_diy_wood.paa";
+	model = "z\addons\dayz_buildings\models\diymanual_wood.p3d";
+	icon = "Ca\misc\data\icons\i_danger_CA.paa";
+	displayName = $STR_BLD_name_ItemDIY_woodGate;//"DIY Manual (Gate's)"
+	descriptionShort = $STR_BLD_desc_ItemDIY_woodGate;//"For wooden Gates"
+
+#ifdef _Devlopment_
+	class ItemActions {
+		class Build {
+			text = $STR_BLD_build_ItemDIY_woodGate;//"Wooden Gate"
+			script = "; ['ItemDIY_Gate','Build'] spawn player_build; r_action_count = r_action_count + 1;";
+			require[] = {"ItemEtool"};
+			consume[] = {"ItemLog"};
+			ghost = "WoodenGate_ghost"; //Ghost model needs to be changed to WoodenGate4 so the player sees the finished fully upgrade model as the ghost
+			create = "WoodenGate_foundation"; //Foundation model is using wooden fence foundation (needs to change)
+		};
+
+	};
+#endif
+};
+
+//Left in place to stop problems with dbs
+class ItemDIY_metal: ItemCore {
+	scope = 2;
+	picture = "\z\addons\dayz_buildings\equip\icon_diy_metal.paa";
+	model = "z\addons\dayz_buildings\models\diymanual_metal.p3d";
+	icon = "Ca\misc\data\icons\i_danger_CA.paa";
+	displayName = $STR_BLD_name_ItemDIY_metal;//"DIY Manual (Metal)"
+	descriptionShort = $STR_BLD_desc_ItemDIY_metal;//"For Metal fences"
+	
+#ifdef _Devlopment_
+	class ItemActions {
+		class Build {
+			text = $STR_BLD_build_ItemDIY_metal;//"Metal fence"
+			script = "; ['ItemDIY_metal','Build'] spawn player_build; r_action_count = r_action_count + 1;";
+			require[] = {"ItemEtool"};
+			consume[] = {"ItemRSJ", "ItemStone", "ItemStone" };
+			ghost = "MetalFence_ghost";
+			create = "MetalFence_1_foundation";
+		};
+	};
+#endif
+};
