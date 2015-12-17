d4bc8d1c72e8d2619b1b1a1c12acdeff0fd4ed3f
AsFoxy <asfoxyy@gmail.com>
2015-12-17 18:30:20 +0200

    Added SA58 RIS attachment

--- SQF/dayz_code/Configs/CfgLoot/Groups/Attachments.hpp
+++ SQF/dayz_code/Configs/CfgLoot/Groups/Attachments.hpp
@@ -14,7 +14,8 @@ AttachmentsEast[] =
 	{Loot_MAGAZINE,		2,		Attachment_SupBizon},
 	{Loot_MAGAZINE,		1,		Attachment_Sup545},
 	{Loot_MAGAZINE,		2,		Attachment_GP25},
-	{Loot_MAGAZINE,		2,		Attachment_Ghillie}
+	{Loot_MAGAZINE,		2,		Attachment_Ghillie},
+	{Loot_MAGAZINE,		3,		Attachment_SA58RIS}
 };
 
 AttachmentsWest[] =
@@ -25,5 +26,6 @@ AttachmentsWest[] =
 	{Loot_MAGAZINE,		3,		Attachment_Ghillie},
 	{Loot_MAGAZINE,		2,		Attachment_M203},
 	{Loot_MAGAZINE,		1,		Attachment_Sup556},
-	{Loot_MAGAZINE,		3,		Attachment_Sup9}
+	{Loot_MAGAZINE,		3,		Attachment_Sup9},
+	{Loot_MAGAZINE,		3,		Attachment_SA58RIS}
 };
