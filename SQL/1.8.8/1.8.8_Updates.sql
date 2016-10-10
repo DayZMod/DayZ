8fdd754daf73355606cfa3d29910f319201bf3e8
ebaydayz <ebaydayz@openmailbox.org>
2016-10-10 15:31:00 -0400

    Replace splint with morphine in player gear SQL updates
    Thanks @Inurosen

--- SQL/1.8.8/1.8.8_Updates.sql
+++ SQL/1.8.8/1.8.8_Updates.sql
@@ -11,3 +11,9 @@ update object_classes set Classname = 'UH1H_2_DZ' where classname = 'UH1H_DZ2';
 update object_data set Classname = 'UH1H_2_DZ' where classname = 'UH1H_DZ2';
 update object_spawns set Classname = 'UH1H_2_DZ' where classname = 'UH1H_DZ2';
 update vehicle_spawns set Classname = 'UH1H_2_DZ' where classname = 'UH1H_DZ2';
+
+-- ----------------------------
+-- Woodensplint now takes two inventory slots, so replace it with an item that takes one (ItemMorphine) to avoid gear loss.
+-- ----------------------------
+UPDATE character_data SET Backpack = REPLACE(Backpack, '"equip_woodensplint"', '"ItemMorphine"') WHERE INSTR(Backpack, '"equip_woodensplint"') > 0;
+UPDATE character_data SET Inventory = REPLACE(Inventory, '"equip_woodensplint"', '"ItemMorphine"') WHERE INSTR(Inventory, '"equip_woodensplint"') > 0;
