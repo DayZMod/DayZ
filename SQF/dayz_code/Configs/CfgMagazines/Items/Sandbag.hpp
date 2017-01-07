98c0b8885962e4a80a999faed2362ea58ced6abf
R4Z0R49 <R4Z0R49@gmail.com>
2017-01-07 10:04:23 +0000

    Replace Etool with Shovel. (etool still needed for base stuff ATM)

--- SQF/dayz_code/Configs/CfgMagazines/Items/Sandbag.hpp
+++ SQF/dayz_code/Configs/CfgMagazines/Items/Sandbag.hpp
@@ -15,7 +15,7 @@ class ItemSandbag : CA_Magazine
 		{
 			text = $STR_ACTION_BUILD;
 			script = "; [_id,'Build'] spawn player_build; r_action_count = r_action_count + 1;";
-			require[] = {"ItemEtool"};
+			require[] = {"ItemShovel"};
 			consume[] = {"ItemSandbag"};
 			create = "Sandbag1_DZ";
 			//bypass RoadCheck
@@ -26,7 +26,7 @@ class ItemSandbag : CA_Magazine
 		{
 			text = $STR_CREATE_STASH;
 			script = "; [_id,'Build2'] spawn player_build; r_action_count = r_action_count + 1;";
-			require[] = {"ItemEtool"};
+			require[] = {"ItemShovel"};
 			consume[] = {"ItemSandbag"};
 			create = "StashSmall";
 			//Bypass collision test
