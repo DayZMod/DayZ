947235364299b3e51cc8c8def762cf3bfed8329d
R4Z0R49 <R4Z0R49@gmail.com>
2017-02-05 18:57:10 +0000

    Added SQL 1.8.9,
    Changes fence maintenance requirements.

---
+++ SQL/1.8.9/randomizeVehicleInventory.sql
@@ -0,0 +1,106 @@
+/*!50003 DROP FUNCTION IF EXISTS `randomizeVehicleInventory` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`dayzhivemind`@`%` FUNCTION `randomizeVehicleInventory`(`c` varchar(255)) RETURNS longtext CHARSET latin1
+    READS SQL DATA
+BEGIN
+	DECLARE WeaponClasses		VARCHAR(255);
+	DECLARE WeaponAmounts		VARCHAR(255);
+	DECLARE MagazineClasses		VARCHAR(512); #changed to 512 since vehicle inventory can be large
+	DECLARE MagazineAmounts		VARCHAR(255);
+	DECLARE BackpackClasses		VARCHAR(255);
+	DECLARE BackpackAmounts		VARCHAR(255);
+	
+	DECLARE InventoryID INT DEFAULT (SELECT Inventory FROM vehicle_spawns WHERE Classname = c LIMIT 1);
+	
+	DECLARE MaxWeapons		INT DEFAULT (SELECT MaxWeapons		FROM vehicle_spawns WHERE Classname = c LIMIT 1);
+	DECLARE MaxMagazines	INT DEFAULT (SELECT MaxMagazines	FROM vehicle_spawns WHERE Classname = c LIMIT 1);
+	DECLARE MaxBackpacks	INT DEFAULT (SELECT MaxBackpacks	FROM vehicle_spawns WHERE Classname = c LIMIT 1);
+	
+	IF (ISNULL(InventoryID)) THEN
+		RETURN "[]";
+	END IF;
+	
+	#Weapons
+	SET @amt := 0;
+	SET @sum := 0;
+	SELECT	GROUP_CONCAT("\"", Classname, "\""),
+			GROUP_CONCAT(IF(@sum > MaxWeapons, MaxWeapons - @sum + @amt, @amt))
+	INTO WeaponClasses, WeaponAmounts
+	FROM 
+	(
+		SELECT *
+		FROM vehicle_inventory
+		WHERE ID = InventoryID
+			AND Type = "Weapon" 
+			AND RAND() < Chance
+		ORDER BY RAND()
+	) AS x
+	WHERE (@amt := ROUND(x.MinAmount + RAND() * (x.MaxAmount - x.MinAmount))) IS NOT NULL
+		AND (@sum := @sum + @amt) IS NOT NULL
+		AND @sum - @amt < MaxWeapons;
+	
+	#Magazines
+	SET @amt := 0;
+	SET @sum := 0;
+	SELECT	GROUP_CONCAT("\"", Classname, "\""),
+			GROUP_CONCAT(IF(@sum > MaxMagazines, MaxMagazines - @sum + @amt, @amt))
+	INTO MagazineClasses, MagazineAmounts
+	FROM 
+	(
+		SELECT *
+		FROM vehicle_inventory 
+		WHERE ID = InventoryID
+			AND Type = "Magazine" 
+			AND RAND() < Chance
+		ORDER BY RAND()
+	) AS x
+	WHERE (@amt := ROUND(x.MinAmount + RAND() * (x.MaxAmount - x.MinAmount))) IS NOT NULL
+		AND (@sum := @sum + @amt) IS NOT NULL
+		AND @sum - @amt < MaxMagazines;
+	
+	#Backpacks
+	SET @amt := 0;
+	SET @sum := 0;
+	SELECT	GROUP_CONCAT("\"", Classname, "\""),
+			GROUP_CONCAT(IF(@sum > MaxBackpacks, MaxBackpacks - @sum + @amt, @amt))
+	INTO BackpackClasses, BackpackAmounts
+	FROM 
+	(
+		SELECT *
+		FROM vehicle_inventory 
+		WHERE ID = InventoryID
+			AND Type = "Backpack" 
+			AND RAND() < Chance
+		ORDER BY RAND()
+	) AS x
+	WHERE (@amt := ROUND(x.MinAmount + RAND() * (x.MaxAmount - x.MinAmount))) IS NOT NULL
+		AND (@sum := @sum + @amt) IS NOT NULL
+		AND @sum - @amt < MaxBackpacks;
+	
+	#Concatenate all classes and amounts into one string in the format:
+	#[[[<backpack classes>],[<weapon amounts>]],[[<magazine classes>],[<magazine amounts>]],[[<backpack classes>],[<backpack amounts>]]]
+	RETURN CONCAT_WS
+	(
+		"",
+		"[[[",			WeaponClasses,
+		"], [",			WeaponAmounts,
+		"]], [[",		MagazineClasses,
+		"], [",			MagazineAmounts,
+		"]], [[",		BackpackClasses,
+		"], [",			BackpackAmounts,
+		"]]]"
+	);
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
---
+++ SQL/1.8.9/vehicle_spawns.sql
@@ -0,0 +1,90 @@
+--
+-- Table structure for table `vehicle_spawns`
+--
+
+DROP TABLE IF EXISTS `vehicle_spawns`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vehicle_spawns` (
+  `ID` int(11) NOT NULL AUTO_INCREMENT,
+  `Classname` varchar(255) NOT NULL,
+  `Chance` double NOT NULL,
+  `MaxNum` int(11) NOT NULL DEFAULT '1',
+  `Location` int(11) NOT NULL,
+  `Inventory` int(11) DEFAULT NULL,
+  `Hitpoints` int(11) DEFAULT NULL,
+  `MinDamage` double NOT NULL,
+  `MaxDamage` double NOT NULL,
+  `MinFuel` double NOT NULL,
+  `MaxFuel` double NOT NULL,
+  `MaxWeapons` int(11) NOT NULL,
+  `MaxMagazines` int(11) NOT NULL,
+  `MaxBackpacks` int(11) NOT NULL,
+  `CleanupTime` int(11) NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE KEY `Classname` (`Classname`) USING HASH
+) ENGINE=MyISAM AUTO_INCREMENT=39 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle_spawns`
+--
+
+LOCK TABLES `vehicle_spawns` WRITE;
+/*!40000 ALTER TABLE `vehicle_spawns` DISABLE KEYS */;
+INSERT INTO `vehicle_spawns` (`ID`, `Classname`, `Chance`, `MaxNum`, `Location`, `Inventory`, `Hitpoints`, `MinDamage`, `MaxDamage`, `MinFuel`, `MaxFuel`, `MaxWeapons`, `MaxMagazines`, `MaxBackpacks`, `CleanupTime`) VALUES 
+(1, 'UAZ_Unarmed_TK_EP1', 0.7, 2, 7, 9, 1, 0.2, 0.8, 0, 0.8, 10, 50, 8, 300),
+(2, 'UAZ_Unarmed_UN_EP1', 0.59, 2, 10, 8, 1, 0.2, 0.8, 0, 0.8, 10, 50, 8, 300),
+(3, 'UAZ_RU', 0.59, 1, 7, 9, 1, 0.2, 0.8, 0, 0.8, 10, 50, 8, 300),
+(4, 'UAZ_Unarmed_TK_CIV_EP1', 0.59, 3, 10, 9, 1, 0.2, 0.8, 0, 0.8, 10, 50, 8, 300),
+(5, 'SkodaBlue', 0.68, 2, 10, 20, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(6, 'Skoda', 0.68, 2, 10, 20, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(7, 'SkodaGreen', 0.68, 2, 10, 20, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(8, 'ATV_US_EP1', 0.7, 6, 0, 21, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 120),
+(9, 'TT650_Ins', 0.72, 1, 1, 2, 2, 0.2, 0.8, 0, 0.8, 0, 5, 0, 120),
+(10, 'TT650_TK_CIV_EP1', 0.72, 3, 1, 2, 2, 0.2, 0.8, 0, 0.8, 0, 5, 0, 120),
+(11, 'Old_bike_TK_CIV_EP1', 0.64, 8, 6, NULL, NULL, 0.2, 0.8, 0, 0, 0, 0, 0, 60),
+(12, 'hilux1_civil_3_open', 0.59, 3, 10, 11, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(13, 'Ikarus', 0.59, 2, 11, 6, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 180),
+(14, 'Ikarus_TK_CIV_EP1', 0.59, 2, 11, 6, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 180),
+(15, 'Tractor', 0.7, 3, 12, 22, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 60),
+(16, 'S1203_TK_CIV_EP1', 0.69, 2, 10, 1, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(17, 'V3S_Civ', 0.66, 2, 13, 17, 1, 0.2, 0.8, 0, 0.8, 50, 200, 8, 300),
+(18, 'UralCivil', 0.59, 2, 13, 18, 1, 0.2, 0.8, 0, 0.8, 50, 200, 8, 300),
+(19, 'car_hatchback', 0.73, 2, 10, 19, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(20, 'Fishing_Boat', 0.61, 1, 14, 16, 2, 0.2, 0.8, 0, 0.8, 40, 400, 8, 300),
+(21, 'PBX', 0.59, 1, 14, 4, 2, 0.2, 0.8, 0, 0.8, 5, 50, 2, 300),
+(22, 'Smallboat_1', 0.59, 2, 14, 16, 2, 0.2, 0.8, 0, 0.8, 20, 200, 4, 300),
+(23, 'Volha_1_TK_CIV_EP1', 0.71, 3, 10, 20, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(24, 'Volha_2_TK_CIV_EP1', 0.71, 3, 10, 20, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(25, 'SUV_DZ', 0.59, 1, 10, 3, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(26, 'car_sedan', 0.59, 2, 10, 19, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(27, 'UH1H_DZ', 0.59, 2, 2, 13, 3, 0.2, 0.8, 0, 0.8, 5, 25, 4, 900),
+(28, 'AH6X_DZ', 0.48, 3, 3, 7, 4, 0.2, 0.8, 0, 0.8, 3, 30, 2, 900),
+(29, 'AN2_DZ', 0.6, 1, 5, 14, NULL, 0, 0, 0.1, 0.4, 10, 80, 15, 900),
+(30, 'BAF_Offroad_D', 0.54, 2, 7, 10, 5, 0.2, 0.8, 0, 0.8, 10, 90, 2, 300),
+(31, 'BAF_Offroad_W', 0.54, 2, 7, 10, 5, 0.2, 0.8, 0, 0.8, 10, 90, 2, 300),
+(32, 'MH6J_DZ', 0.48, 2, 3, 7, 3, 0.2, 0.8, 0, 0.8, 3, 30, 2, 900),
+(33, 'HMMWV_DZ', 0.21, 2, 7, 5, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(34, 'Pickup_PK_INS', 0.1, 2, 7, 12, 6, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(35, 'Offroad_DSHKM_INS', 0.07, 1, 7, 12, 6, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(36, 'AN2_2_DZ', 1, 1, 5, 15, NULL, 0, 0, 0.1, 0.4, 10, 80, 15, 900),
+(37, 'UH1H_2_DZ', 0.3, 1, 2, 13, 3, 0.2, 0.8, 0, 0.8, 5, 25, 4, 900),
+(38, 'M1030', 0.72, 1, 1, 2, 2, 0.2, 0.8, 0, 0.8, 0, 5, 0, 120),
+(39, 'Old_moto_TK_Civ_EP1', 0.72, 1, 1, 2, 2, 0.2, 0.8, 0, 0.8, 0, 5, 0, 120),
+(40, 'UralCivil2', 0.66, 2, 13, 17, 1, 0.2, 0.8, 0, 0.8, 50, 200, 8, 300),
+(41, 'Ural_TK_CIV_EP1', 0.66, 2, 13, 17, 1, 0.2, 0.8, 0, 0.8, 50, 200, 8, 300),
+(42, 'Ural_CDF', 0.66, 2, 13, 17, 1, 0.2, 0.8, 0, 0.8, 50, 200, 8, 300),
+(43, 'Ural_INS', 0.66, 2, 13, 17, 1, 0.2, 0.8, 0, 0.8, 50, 200, 8, 300),
+(44, 'UralOpen_INS', 0.66, 2, 13, 17, 1, 0.2, 0.8, 0, 0.8, 50, 200, 8, 300),
+(45, 'LandRover_TK_CIV_EP1', 0.54, 2, 7, 10, 5, 0.2, 0.8, 0, 0.8, 10, 90, 2, 300),
+(46, 'LandRover_CZ_EP1', 0.54, 2, 7, 10, 5, 0.2, 0.8, 0, 0.8, 10, 90, 2, 300),
+(47, 'datsun1_civil_2_covered', 0.59, 2, 10, 19, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(48, 'datsun1_civil_3_open', 0.59, 2, 10, 19, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(49, 'hilux1_civil_1_open', 0.59, 2, 10, 19, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(50, 'hilux1_civil_2_covered', 0.59, 2, 10, 19, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(51, 'VolhaLimo_TK_CIV_EP1', 0.59, 2, 10, 19, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(52, 'ladalm', 0.59, 2, 10, 19, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300),
+(53, 'lada2', 0.59, 2, 10, 19, 1, 0.2, 0.8, 0, 0.8, 10, 50, 2, 300);
+/*!40000 ALTER TABLE `vehicle_spawns` ENABLE KEYS */;
+UNLOCK TABLES;
