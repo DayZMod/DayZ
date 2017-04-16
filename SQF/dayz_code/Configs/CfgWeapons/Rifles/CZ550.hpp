8dc4f223462718c11542c29199edc50fc9df5f47
Streatman <streatman@t-online.de>
2017-04-16 13:54:41 +0200

    Changes for CZ550 and LeeEnfield
    Changed the scope to the orriginal Stepscope
    Optimized animations for CZ550
    Added Boltaction animation to LeeEnfield

--- SQF/dayz_code/Configs/CfgWeapons/Rifles/CZ550.hpp
+++ SQF/dayz_code/Configs/CfgWeapons/Rifles/CZ550.hpp
@@ -6,10 +6,10 @@ class CZ550_DZ : Huntingrifle
 	//CZ550_SCOPE
 	optics = true;
 	modelOptics = "\ca\weapons\2Dscope_Hunter_12";
-
+	
 	class OpticsModes
 	{
-		class Scope
+		class scope
 		{
 			opticsID = 1;
 			useModelOptics = true;
@@ -17,29 +17,32 @@ class CZ550_DZ : Huntingrifle
 			opticsDisablePeripherialVision = true;
 			opticsZoomMin = 0.0249;
 			opticsZoomMax = 0.0711;
-			opticsZoomInit= 0.0711;
-			distanceZoomMin= 300;
-			distanceZoomMax= 300;
+			opticsZoomInit = 0.0711;
+			discreteDistance[] = {100,200,300,400,500,600,700,800};
+			discreteDistanceInitIndex = 2;
+			discreteFov[] = {0.0711,0.0249};
+			discreteInitIndex = 0;
+			distanceZoomMin = 300;
+			distanceZoomMax = 300;
 			memoryPointCamera = "opticView";
 			visionMode[] = {"Normal"};
-			opticsPPEffects[]={"OpticsCHAbera2","OpticsBlur2"};
+			opticsPPEffects[] = {"OpticsCHAbera1","OpticsBlur1"};
 			cameraDir = "";
 		};
-		
 		class Ironsights
 		{
 			opticsID = 2;
 			useModelOptics = false;
 			opticsFlare = false;
 			opticsDisablePeripherialVision = false;
-			opticsZoomMin=0.25;
-			opticsZoomMax=1.1;
-			opticsZoomInit=0.5;
-			distanceZoomMin= 100;
-			distanceZoomMax= 100;
+			opticsZoomMin = 0.25;
+			opticsZoomMax = 1.1;
+			opticsZoomInit = 0.5;
+			distanceZoomMin = 100;
+			distanceZoomMax = 100;
 			memoryPointCamera = "eye";
 			visionMode[] = {};
-			opticsPPEffects[]={};
+			opticsPPEffects[] = {};
 			cameraDir = "";
 		};
 	};
