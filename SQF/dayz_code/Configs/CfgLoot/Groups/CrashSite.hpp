799882ee6695cc6f8826730760acca5c98d418e7
R4Z0R49 <R4Z0R49@gmail.com>
2017-05-24 14:07:55 +0100

    C4 Changes to crashsites.

--- SQF/dayz_code/Configs/CfgLoot/Groups/CrashSite.hpp
+++ SQF/dayz_code/Configs/CfgLoot/Groups/CrashSite.hpp
@@ -31,7 +31,7 @@ CrashSiteRU[] =
 	//Tools
 	{Loot_WEAPON,		1.5,	ItemGPS},
 	{Loot_WEAPON,		1,		Binocular_Vector},
-	{Loot_MAGAZINE,		1.5,	PipeBomb},
+	{Loot_MAGAZINE,		0.5,	PipeBomb},
 	
 	//Backpacks
 	
@@ -92,7 +92,7 @@ CrashSiteUS[] =
 	//Tools
 	{Loot_WEAPON,		1.5,	ItemGPS},
 	{Loot_WEAPON,		1,		Binocular_Vector},
-	{Loot_MAGAZINE,		2,	PipeBomb},
+	{Loot_MAGAZINE,		0.7,	PipeBomb},
 	
 	//Backpacks
 	
@@ -153,7 +153,7 @@ CrashSiteEU[] =
 	//Tools
 	{Loot_WEAPON,		1.5,	ItemGPS},
 	{Loot_WEAPON,		1,		Binocular_Vector},
-	{Loot_MAGAZINE,		2,	PipeBomb},
+	{Loot_MAGAZINE,		0.8,	PipeBomb},
 	
 	//Backpacks
 	
@@ -214,7 +214,7 @@ CrashSiteUN[] =
 	
 	//Tools
 	{Loot_WEAPON,		2,		ItemGPS},
-	{Loot_MAGAZINE,		3,		PipeBomb},
+	{Loot_MAGAZINE,		1,		PipeBomb},
 	
 	//Clothing
 	{Loot_MAGAZINE,		2,		Skin_Camo1_DZ},
