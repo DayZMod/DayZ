18af843d233e3d36ce885ce14521bfdb2455a1a2
Streatman <streatman@t-online.de>
2017-06-27 19:04:00 +0200

    Added sounds for the mounted machine guns

---
+++ SQF/dayz_code/Configs/CfgWeapons/Rifles/PKT.hpp
@@ -0,0 +1,10 @@
+reloadMagazineSound[] = {"\z\addons\dayz_communityweapons\PKM\sounds\Reload",1,1,35};
+drySound[] = 			{"\z\addons\dayz_communityweapons\PKM\sounds\Dry",0.01,1,10};
+class manual: MGun
+{
+	begin1[] = {"\z\addons\dayz_communityweapons\PKM\sounds\PKM_s1",5,1,1500};
+	begin2[] = {"\z\addons\dayz_communityweapons\PKM\sounds\PKM_s2",5,1,1500};
+	begin3[] = {"\z\addons\dayz_communityweapons\PKM\sounds\PKM_s3",5,1,1500};
+	begin4[] = {"\z\addons\dayz_communityweapons\PKM\sounds\PKM_s4",5,1,1500};
+	soundBegin[] = {"begin1",0.25,"begin2",0.25,"begin3",0.25,"begin4",0.25};
+};
