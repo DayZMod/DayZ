dc4f042c4780fc455d6a407b13beb529a85cbd71
Streatman <streatman@t-online.de>
2017-07-02 14:41:54 +0200

    SUV

--- SQF/dayz_code/Configs/CfgVehicles/Land/SUV.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Land/SUV.hpp
@@ -1,4 +1,25 @@
-class SUV_DZ: SUV_Base_EP1 {
+class SUV_Base_EP1 : Car
+{
+	class HitPoints : HitPoints
+	{
+		class HitLFWheel;
+		class HitLBWheel;
+		class HitRFWheel;
+		class HitRBWheel;
+		class HitFuel;
+		class HitEngine;
+
+		//armored glass - hight armor value
+		class HitGlass1:HitGlass1 {armor=1;};
+		class HitGlass2:HitGlass2 {armor=1;};
+		class HitGlass3:HitGlass3 {armor=1;};
+		class HitGlass4:HitGlass4 {armor=1;};
+	};
+	#include "sounds\SUV.hpp"
+};
+
+class SUV_DZ : SUV_Base_EP1 
+{
 	crew = "";
 	faction = "BIS_TK_CIV";
 	rarityurban = 0.8;
@@ -7,71 +28,82 @@ class SUV_DZ: SUV_Base_EP1 {
 	typicalCargo[] = {};
 	class TransportMagazines {};
 	class TransportWeapons {};
-	class HitPoints : HitPoints {
-		class HitLFWheel:HitLFWheel {
+	class HitPoints : HitPoints 
+	{
+		class HitLFWheel : HitLFWheel 
+		{
 			armor = 0.15;
 			material = -1;
 			name = "wheel_1_1_steering";
 			passthrough = 0.3;
 			visual = "";
 		};
-		class HitLBWheel:HitLBWheel {
+		class HitLBWheel : HitLBWheel 
+		{
 			armor = 0.15;
 			material = -1;
 			name = "wheel_1_2_steering";
 			passthrough = 0.3;
 			visual = "";
 		};
-		class HitRFWheel:HitRFWheel {
+		class HitRFWheel : HitRFWheel 
+		{
 			armor = 0.15;
 			material = -1;
 			name = "wheel_2_1_steering";
 			passthrough = 0.3;
 			visual = "";
 		};
-		class HitRBWheel:HitRBWheel {
+		class HitRBWheel : HitRBWheel 
+		{
 			armor = 0.15;
 			material = -1;
 			name = "wheel_2_2_steering";
 			passthrough = 0.3;
 			visual = "";
 		};
-		class HitFuel:HitFuel {
+		class HitFuel : HitFuel 
+		{
 			armor = 0.14;
 			material = -1;
 			name = "palivo";
 			passthrough = 1;
 			visual = "";
 		};
-		class HitEngine:HitEngine {
+		class HitEngine : HitEngine 
+		{
 			armor = 0.5;
 			material = -1;
 			name = "motor";
 			passthrough = 1;
 			visual = "";
 		};
-		class HitGlass1:HitGlass1 {
+		class HitGlass1 : HitGlass1 
+		{
 			armor = 0.1;
 			material = -1;
 			name = "glass1";
 			passthrough = 0;
 			visual = "glass1";
 		};
-		class HitGlass2:HitGlass2 {
+		class HitGlass2 : HitGlass2 
+		{
 			armor = 0.1;
 			material = -1;
 			name = "glass2";
 			passthrough = 0;
 			visual = "glass2";
 		};
-		class HitGlass3:HitGlass3 {
+		class HitGlass3 : HitGlass3 
+		{
 			armor = 0.1;
 			material = -1;
 			name = "glass3";
 			passthrough = 0;
 			visual = "glass3";
 		};
-		class HitGlass4:HitGlass4 {
+		class HitGlass4  :HitGlass4 
+		{
 			armor = 0.1;
 			material = -1;
 			name = "glass4";
