852ed578572828aa728e3bd7a73abcc29f850b6e
Streatman <streatman@t-online.de>
2017-07-02 14:40:16 +0200

    Littlebird

--- SQF/dayz_code/Configs/CfgVehicles/Air/LittleBird.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Air/LittleBird.hpp
@@ -1,3 +1,8 @@
+class AH6_Base_EP1: Helicopter
+{
+	#include "sounds\LittleBird.hpp"
+};
+
 class AH6X_DZ_Base: AH6_Base_EP1
 {
 	class Helicopter: Air
@@ -42,5 +47,34 @@ class AH6X_DZ: AH6_Base_EP1
 	transportMaxWeapons = 3;
 	transportMaxMagazines = 30;
 	transportmaxbackpacks = 2;
+	class Turrets {};
+};
+
+class MH6J_DZ: AH6_Base_EP1
+{
+	scope = 2;
+	side = 2;
+	crew = "";
+	enablemanualfire = 0;
+	typicalCargo[] = {};
+	displayname = $STR_VEH_NAME_MH6J;
+	displaynameshort = $STR_EP1_DN_MH6J;
+	hiddenselections[] = {"camo1", "camo2"};
+	transportsoldier = 5;
+	hiddenselectionstextures[] = {"ca\air_e\ah6j\data\ah6_merge1_co.paa", "ca\air_e\ah6j\data\default_co.paa"};
+	icon = "\ca\air_e\data\UI\Icon_mh6j_CA.paa";
+	model = "\ca\air_e\ah6j\mh6j";
+	picture = "\ca\air_e\data\UI\Picture_mh6j_CA.paa";
+	radartype = 0;
+	class TransportMagazines{};
+	class TransportWeapons{};
+	weapons[] = {};
+	magazines[] = {};
+	commanderCanSee = 2+16+32;
+	gunnerCanSee = 2+16+32;
+	driverCanSee = 2+16+32;
+	transportMaxWeapons = 3;
+	transportMaxMagazines = 20;
+	transportmaxbackpacks = 3;
 	class Turrets {};
 };
