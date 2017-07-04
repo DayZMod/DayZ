194479c98e08ae2bf36911496752b5d55455ae8f
Streatman <streatman@t-online.de>
2017-07-04 20:15:08 +0200

    Increased the angle of the headlights of cars & added headlight to bicycle

--- SQF/dayz_code/Configs/CfgVehicles/Car/V3S_Civ.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Car/V3S_Civ.hpp
@@ -1,13 +1,26 @@
-class V3S_Civ: V3S_Base {
-		crew = "";
-		faction = "CIV";
-		rarityurban = 0.3;
-		scope = public;
-		side = 3;
-		typicalCargo[] = {};
-		class TransportMagazines {};
-		class TransportWeapons {};
-		class Library {
-			libtextdesc = $STR_EP1_LIB_V3S;
+class V3S_Civ: V3S_Base 
+{
+	crew = "";
+	faction = "CIV";
+	rarityurban = 0.3;
+	scope = public;
+	side = 3;
+	typicalCargo[] = {};
+	class TransportMagazines {};
+	class TransportWeapons {};
+	class Library 
+	{
+		libtextdesc = $STR_EP1_LIB_V3S;
+	};
+	class Reflectors
+	{
+		class Left
+		{
+			angle = 120;
+		};
+		class Right
+		{
+			angle = 120;
 		};
 	};
+};
