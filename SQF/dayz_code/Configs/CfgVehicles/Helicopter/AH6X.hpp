a1bd05b02c446a8a8d62b7c01b6fe970d923ecf2
Streatman <streatman@t-online.de>
2017-11-05 21:11:35 +0100

    deleted the model lines in dayz code

--- SQF/dayz_code/Configs/CfgVehicles/Helicopter/AH6X.hpp
+++ SQF/dayz_code/Configs/CfgVehicles/Helicopter/AH6X.hpp
@@ -17,10 +17,8 @@ class AH6X_DZ_Base: AH6_Base_EP1
 
 class AH6X_DZ: AH6_Base_EP1
 {
-	model = "z\addons\dayz_vehicles\helicopters\GreyBird\GreyBird.p3d";
 	displayname = $STR_VEH_NAME_AH6X;
 	displaynameshort = $STR_EP1_DN_AH6X;
-	cargoAction[] = {"AH6j_Cargo_EP1","UH1Y_Cargo01","UH1Y_Cargo01"};
 	audible = 6;
 	enablemanualfire = 0;
 	scope = 2;
