5e3de2b7dfe7bd0fa4560b61a4fd9fc4cf353ca9
R4Z0R49 <R4Z0R49@gmail.com>
2017-11-06 07:45:21 +0000

    Fixed path issues from pull request

--- SQF/dayz_vehicles/config.cpp
+++ SQF/dayz_vehicles/config.cpp
@@ -9,16 +9,3 @@ class CfgPatches
 	};
 
 };
-class CfgVehicles 
-{	
-	class UH1H_base;
-	class UH1H_DZ : UH1H_base
-	{
-		model = "z\addons\dayz_vehicles\helicopters\huey\huey.p3d";
-	};
-	class AH6_Base_EP1;
-	class AH6X_DZ : AH6_Base_EP1
-	{
-		model = "z\addons\dayz_vehicles\helicopters\greybird\greybird.p3d";
-	};
-};
